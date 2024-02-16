f78dfe6a69336bfcb3d3120697b085a00f8a6a3d
Cristian Metz <cristianrmetz@gmail.com>
2024-02-16 03:20:25 -0300

    add media query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,17 @@ import LinkIcon from "./LinkIcon";
 import { faGithub, faLinkedin, faInstagram} from '@fortawesome/free-brands-svg-icons';
 
 function Header(props) {
+
     return (
         <header>
             <LinkIcon link="https://www.linkedin.com/in/cristian-rafael-metz-2b28931b3/" icon={faLinkedin} />
             <LinkIcon link="https://github.com/crmetz" icon={faGithub} />
             <LinkIcon link="https://www.instagram.com/cristianmetz_/" icon={faInstagram} />
-            <Button text="My resume" />
+            <a href="https://drive.google.com/uc?export=download&id=1yf6eVUKIMnJoOlzdS1aw4FDFh3sx-DSC"><Button onClick text="My resume" /></a>
+            
         </header>
+
+
     )
    
 }
